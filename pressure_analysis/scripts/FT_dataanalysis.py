fab80a950006848a4cbeffd389616ce248f859f0
chiara <chiara.it98@gmail.com>
2024-03-06 10:36:30 +0100

    added a second bandstop filter

--- pressure_analysis/scripts/FT_dataanalysis.py
+++ pressure_analysis/scripts/FT_dataanalysis.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 from scipy.fft import fft, fftfreq
 from scipy.optimize import curve_fit
-from scipy.signal import butter, freqs, lfilter, sosfilt
+from scipy.signal import butter, sosfilt, sosfiltfilt
 
 from labviewdataprocessing.labviewdatareading import LabViewdata_reading
 
@@ -64,7 +64,7 @@ if __name__ == "__main__":
     #T_eff = T_eff + 273.15 #Kelvin
 
     #Looking at overall data - P4, T5, T_room
-    fig, axs = plt.subplots(5)
+    fig, axs = plt.subplots(4)
     fig.suptitle(fr'Absolute pressure inside AC and corresponding temperature - Gas DME,$T_{{Julabo}}$ = {np.mean(TJ):.2f}°C')
     axs[0].plot(timestamps, P4, color='firebrick')
     axs[0].set(xlabel=r'Timestamp', ylabel=r'$P_4$ [mbar]')
@@ -97,8 +97,12 @@ if __name__ == "__main__":
     Pft = fft((P4-np.mean(P4))/(np.max(P4)-np.min(P4))) #DFT of effective pressure
 
     #Constructing a lowpass Butterworth filter with defined cutoff frequency
-    f_cutoff = 1/(1*3600) #Hz
+    f_cutoff = 1/(1.5*3600) #Hz
     sos = butter(4, f_cutoff, 'low', fs=1/T, analog=False, output='sos')
+    all_sos = [sos]
+    sos2 = butter(2, [0.0002,0.0003], 'bandstop', fs=1/T, analog=False, output='sos')
+    all_sos.append(sos2)
+    sos = np.vstack(all_sos)
     T_butterworth = sosfilt(sos, T_eff-T_eff[0]) #It is needed to shift data in order to let them start from 0
     T_butter_ft = fft((T_butterworth-np.mean(T_butterworth))/(np.max(T_butterworth)-np.min(T_butterworth))) #DFT of T_butterworth
 
@@ -126,22 +130,24 @@ if __name__ == "__main__":
     #Plotting pressure and Butterworth filtered effective temperature in time domain
     fig, ax1 = plt.subplots()
     ax1.set_xlabel('Time from new $T_{Julabo}$ settings [hours]')
-    ax1.set_ylabel(r'$P_{4}$ [mbar]', color='red')
+    ax1.set_ylabel(r'$P_4 - P_{4,fitted}$ [mbar]', color='red')
     ax1.plot(t_hours, (P4-expo(t_hours,*popt))/(np.max(P4)-np.min(P4)),\
-            color='red', label=r'$P_{4}$ [mbar]')
+            color='red', label=r'$P_4 - P_{4,fitted}$ [mbar]')
     ax1.tick_params(axis='y', labelcolor='red')
     ax2 = ax1.twinx()  # instantiate a second pair of axes that shares the same x-axis
     ax2.set_ylabel(r'$T_{eff,filtered}$ [°C]', color='steelblue')
+    ax2.plot(t_hours, (T_eff-T_eff[0])/((max(T_eff)-min(T_eff))), alpha=0.5, label=r'$T_{eff}$ [°C]')
     ax2.plot(t_hours, T_butterworth/((max(T_butterworth)-min(T_butterworth))),\
-            label=fr'T_{{eff}} Butterworth filtered , $f_{{cutoff}} = {f_cutoff:.2e}$ [Hz]')
+            label=fr'T_{{eff}} Butterworth filtered [°C], $f_{{cutoff}} = {f_cutoff:.2e}$ [Hz]')
     ax2.tick_params(axis='y', labelcolor='steelblue')
     fig.legend()
 
     #Plotting hysteresis curves in ordert to understand if time lag has been compensated
     plt.figure('P-T curves')
     #Plotting pressures in Pa and temperatures in Kelvin
+    plt.errorbar((P4-expo(t_hours,*popt))*100, (T_eff-T_eff[0])+273.15, label=r'$T_{eff}$')
     plt.errorbar((P4-expo(t_hours,*popt))*100, T_butterworth+273.15, label=r'$T_{eff,filtered}$')
-    plt.xlabel(r'$P_4$ [Pa]')
+    plt.xlabel(r'$P_4 - P_{4,fitted}$ [Pa]')
     plt.ylabel(r'$T_{eff,filtered}$ [K]')
     plt.legend()
     plt.grid()
@@ -154,10 +160,11 @@ if __name__ == "__main__":
     t=t_hours
     popt, pcov = curve_fit(expo, t, p_over_tfiltered, p0=[*popt])
     print(f'Optimal parameters: \n\
-          A0 = {popt[0]} +/- {np.sqrt(pcov[0][0])} [mbar],\n\
+          A0 = {popt[0]} +/- {np.sqrt(pcov[0][0])} [Pa/K],\n\
           tau = {popt[1]} +/- {np.sqrt(pcov[1][1])} [hours],\n\
-          c = {popt[2]} +/- {np.sqrt(pcov[2][2])} [mbar]')
+          c = {popt[2]} +/- {np.sqrt(pcov[2][2])} [Pa/K]')
     plt.figure('P4/T_eff as a function of time')
+    plt.errorbar(t, P4_Pa/(T_eff-T_eff[0]+273.15), alpha=0.5, label=r'$\frac{P_4}{T_{eff}}$')
     plt.errorbar(t, p_over_tfiltered, label=r'$\frac{P_4}{T_{eff,filtered}}$')
     plt.plot(t, expo(t,*popt), label='Exponential fit')
     plt.ylabel(r'$\frac{P_4}{T_{eff,filtered}}$ [$\frac{Pa}{K}$]')
