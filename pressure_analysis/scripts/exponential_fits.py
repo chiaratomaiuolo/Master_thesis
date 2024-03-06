c6abc4417b5420a26b706069a104248f3e01e01d
chiara <chiara.it98@gmail.com>
2024-03-06 18:35:48 +0100

    minor

--- pressure_analysis/scripts/exponential_fits.py
+++ pressure_analysis/scripts/exponential_fits.py
@@ -14,7 +14,7 @@ __description__ = \
 "This script is used for performing the data analysis of some LabView datasets\
 for the study of the behaviour of gases inside Absorption Chamber (AC) of the BFS."
 
-def iterative_exponential_fit(x: np.array, y: np.array, model, mask: np.array, popt: np.array, hours_start:float=3, hours_lag: float=10):
+def iterative_exponential_fit(x: np.array, y: np.array, model, popt: np.array, hours_start:float=3, hours_lag: float=10):
     """Performs an iterative fit on an increasing-lenght dataset until its end.
     At the end of the fit prints the parameters and plots the current fitting 
     curve on the latest figure of the script.
@@ -35,6 +35,7 @@ def iterative_exponential_fit(x: np.array, y: np.array, model, mask: np.array, p
     """
     i = hours_start #states the start of the dataset in terms of hours from first data object
     popts = []
+    hours = [hours_start]
     mask = x < i
     popt_, pcov_ = curve_fit(model, t_hours[mask], P_eq[mask], p0=popt)
 
@@ -42,23 +43,27 @@ def iterative_exponential_fit(x: np.array, y: np.array, model, mask: np.array, p
     popts.append(popt_)
     print(popt_)
     #Plotting pts, fit and residuals for fit of the first 3 hours
-    fig, axs = plot_with_residuals(t_hours[mask], P_eq[mask], triple_expo_P0_frozen(t_hours[mask], *popt_))
+    fig, axs = plot_with_residuals(t_hours[mask], P_eq[mask], model(t_hours[mask], *popt_))
     axs[0].set(xlabel=r'Time [hours]', ylabel=r'$P_{eq}$ [mbar]')
     axs[0].grid(True)
     axs[1].set(xlabel=r'Time [hours]', ylabel=r'$P_{eq}$ normalized residuals')
     axs[1].grid(True)
     
-    fig, axs = plt.subplots(2)
+    plt.figure()
+    plt.errorbar(x, y, marker='.', linestyle='', label=f'Data')
+    plt.title(f'Iterative fit with {model.__name__}')
     while mask[-1] == False:
         mask = x < i
+        hours.append(i)
         popt_, pcov_ = curve_fit(model, x[mask], y[mask], p0= popts[-1])
         popts.append(popt_)
         print(f'Optimal parameters of {model.__name__}:\n')
         print(popt_)
-        axs[0].plot(x, model(x, *popt_), label=f'{model.__name__} with $t < {i} [hours]$')
-        #axs[1].errorbar(i, popt[-1], marker='.', linestyle='')
+        plt.plot(x, model(x, *popt_), label=f'{model.__name__} with $t < {i} [hours]$')
         i += hours_lag
-    return popt_, fig, axs
+    plt.grid()
+    plt.legend()
+    return hours, popts
 
 if __name__ == "__main__":
     #Datafiles are briefly descripted above their pathfile line. 
@@ -96,75 +101,16 @@ if __name__ == "__main__":
 
     #Computing the equivalent pressure
     P_eq = (((P4*100)/(T_eff_filtered+273.15))*(T_Julabo+273.15))/100 #mbar
-
-    #We want to exploit the optimal parameter variation as we increase the number
-    #of data. 
-    #Before doing that, let's fit the first hours of data with a single
-    #and then double exp.
-    #With fixed P0.
-    #We want to fix the parameters of the 'fast' exponential in order to 
-    #analyze only parameters of the long term exponential.
-    i=2
-    mask = t_hours < i
-    popt_init, pcov_init = curve_fit(expo_P0_frozen, t_hours[mask], P_eq[mask], p0=[50., 10.])
-    print(f'Optimal parameters of single exp for the first 3 hours of data:\n\
-          Delta = {popt_init[0]} +/- {np.sqrt(pcov_init[0][0])} [mbar],\n\
-          tau = {popt_init[1]} +/- {np.sqrt(pcov_init[1][1])} [hours],')
-    #Plotting pts, fit and residuals for single exponential
-    fig, axs = plot_with_residuals(t_hours[mask], P_eq[mask], expo_P0_frozen(t_hours[mask], *popt_init))
-    axs[0].set(xlabel=r'Time [hours]', ylabel=r'$P_{eq}$ [mbar]')
-    axs[0].grid(True)
-    axs[1].set(xlabel=r'Time [hours]', ylabel=r'$P_{eq}$ normalized residuals')
-    axs[1].grid(True)
-    popt_init, pcov_init = curve_fit(double_exp_P0_frozen, t_hours[mask], P_eq[mask], p0=[50., 10., 100., 100.])
-    print(f'Optimal parameters of double exp for the first 3 hours of data:\n\
-          Delta1 = {popt_init[0]} +/- {np.sqrt(pcov_init[0][0])} [mbar],\n\
-          tau1 = {popt_init[1]} +/- {np.sqrt(pcov_init[1][1])} [hours],\n\
-          Delta2 = {popt_init[2]} +/- {np.sqrt(pcov_init[0][0])} [mbar],\n\
-          tau2 = {popt_init[3]} +/- {np.sqrt(pcov_init[1][1])} [hours],')
+    hours, popts = iterative_exponential_fit(t_hours, P_eq, triple_expo_P0_frozen,\
+                      popt=[4.69301877, 0.27440838, 11.9865074, 2.43070955, 123.03394475,\
+                      55.79114849], hours_start=3, hours_lag=10)
     
-    #Plotting pts, fit and residuals
-    fig, axs = plot_with_residuals(t_hours[mask], P_eq[mask], double_exp_P0_frozen(t_hours[mask], *popt_init))
-    axs[0].set(xlabel=r'Time [hours]', ylabel=r'$P_{eq}$ [mbar]')
-    axs[0].grid(True)
-    axs[1].set(xlabel=r'Time [hours]', ylabel=r'$P_{eq}$ normalized residuals')
-    axs[1].grid(True)
-
-    #Fixing parameters for the first exponential
-    def double_exp_frozen(x, Delta2, tau2):
-        return double_exp_P0_frozen(x, popt_init[0], popt_init[1], Delta2, tau2)
+    #After having computed parameters, let's frozen the first exp
+    def frozen_triple_exp(x, Delta2, tau2, Delta3, tau3):
+        return triple_expo_P0_frozen(x, popts[0][0], popts[0][1], Delta2, tau2, Delta3, tau3)
     
-    #Plotting pts and fit with double exp - 4 free parameters
-    fig, axs = iterative_exponential_fit(t_hours, P_eq, double_exp_P0_frozen, mask, popt_init, hours_start=i, hours_lag=15)
-    fig.suptitle(rf'$P_4$ inside AC with epoxy samples, DME filled - all parameters free')
-    axs[0].errorbar(t_hours, P4, marker='.', alpha=0.5, markeredgewidth=0, linestyle='', color='darkorange', label='Data')
-    axs[0].set(xlabel=r'Time [hours]', ylabel=r'$P_{eq}$')
-    axs[1].set(xlabel=r'Time [hours]', ylabel=r'$\tau_2$ [hours]')
-    axs[0].grid(True)
-    axs[1].grid(True)
-    axs[0].legend()
-
-    #Plotting pts and fit with double exp - 6 free parameters
-    popt_init, pcov_init = curve_fit(triple_expo_P0_frozen, t_hours[mask], P_eq[mask], p0=[10., 1., 36.31935209, 7.28600937, 169.75012577, 155.1271365])
-    fig, axs = iterative_exponential_fit(t_hours, P_eq, triple_expo_P0_frozen, mask, popt_init, hours_start=i, hours_lag=15)
-    fig.suptitle(rf'$P_4$ inside AC with epoxy samples, DME filled - all parameters free')
-    axs[0].errorbar(t_hours, P4, marker='.', alpha=0.5, markeredgewidth=0, linestyle='', color='darkorange', label='Data')
-    axs[0].set(xlabel=r'Time [hours]', ylabel=r'$P_{eq}$')
-    axs[1].set(xlabel=r'Time [hours]', ylabel=r'$\tau_2$ [hours]')
-    axs[0].grid(True)
-    axs[1].grid(True)
-    axs[0].legend()
-
-
-    #Plotting pts and fit with first exp fixed
-    fig, axs = iterative_exponential_fit(t_hours, P_eq, double_exp_frozen, mask, popt_init[2:4], hours_start=i, hours_lag=15)
-    fig.suptitle(rf'$P_4$ inside AC with epoxy samples, DME filled - first exponential frozen')
-    axs[0].errorbar(t_hours, P4, marker='.', alpha=0.5, markeredgewidth=0, linestyle='', color='darkorange', label='Data')
-    axs[0].set(xlabel=r'Time [hours]', ylabel=r'$P_{eq}$')
-    axs[1].set(xlabel=r'Time [hours]', ylabel=r'$\tau_2$ [hours]')
-    axs[0].grid(True)
-    axs[1].grid(True)
-    axs[0].legend()
-
+    hours, popts = iterative_exponential_fit(t_hours, P_eq, frozen_triple_exp,\
+                    popt=[33.86507696, 18.45662877, 186.66209992, 216.86914815],\
+                    hours_start=3, hours_lag=10)
 
     plt.show()
