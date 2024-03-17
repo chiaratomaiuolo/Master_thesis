2aa1312c978054a4652b6a20e8bda90f1c1cbdf6
chiara <chiara.it98@gmail.com>
2024-03-17 10:08:10 +0100

    added models

--- pressure_analysis/pressure_analysis/models.py
+++ pressure_analysis/pressure_analysis/models.py
@@ -8,31 +8,30 @@ from scipy.optimize import curve_fit
 from scipy.signal import butter, sosfilt
 
 
-
-
 def expo(x, P0, Delta, tau):
     """Decreasing single exponential
     """
     return P0 - Delta*(1-np.exp(-x/tau))
 
-def expo_P0_frozen(x, Delta, tau):
-    return expo(x, 1198.8, Delta, tau)
+def empty_AC_exp(t):
+    """Decreasing single exponential with parameters fixed by empty chamber dataset fit.
+    It should correspond to surface asbsorption of DME gas by steel walls of AC.
+    """
+    return -6.230*(1-np.exp(-t/80.57))
+
+def alpha_expo_scale(t, P0, Delta, alpha, tau):
+    """Decreasing exponential with exponent on time dependance. 
+    It should describe processes of subdiffusion (alpha<1) or superdiffusion
+    (alpha>1).
+    """
+    return P0 - (Delta*(1-np.exp(-((t/tau)**alpha))))
 
 def double_expo(x, P0, Delta1, tau1, Delta2, tau2):
     """Decreasing double exponential
     """
     return P0 - (Delta1*(1-np.exp(-x/tau1)) + Delta2*(1-np.exp(-x/tau2)))
 
-def double_exp_P0_frozen(x, Delta1, tau1, Delta2, tau2):
-    return double_expo(x, 1198.8, Delta1, tau1, Delta2, tau2)
-
-
 def triple_expo(x, P0, Delta1, tau1, Delta2, tau2, Delta3, tau3):
     """Decreasing triple exponential
     """
     return P0 - (Delta1*(1-np.exp(-x/tau1)) + Delta2*(1-np.exp(-x/tau2)) + Delta3*(1-np.exp(-x/tau3)))
-
-def triple_expo_P0_frozen(x, Delta1, tau1, Delta2, tau2, Delta3, tau3):
-    """Decreasing triple exponential
-    """
-    return triple_expo(x, 1198.8, Delta1, tau1, Delta2, tau2, Delta3, tau3)
